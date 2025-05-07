bd00c814ca72b0ea5d5fff61aa1c681a9eed8c7f
Sofia-Kaliberda <sofiakaliberda022@gmail.com>
2025-05-07 16:01:29 +0300

    add all 4_ homework

---
+++ homework4_2.py
@@ -0,0 +1,10 @@
+arr = [0, 1, 7, 2, 4, 8]
+
+result = 0
+if arr:
+    sum_even = 0
+    for i in range(0, len(arr), 2):
+        sum_even += arr[i]
+    result = sum_even * arr[-1]
+
+print("result:", result)
